77834be04e8fcc1bbda2a8340b0fa874e6056c47
goneone <kuyt1819@gmail.com>
2020-04-19 14:05:23 +0900

    ttaa

---
+++ src/algorithm/N10773.java
@@ -0,0 +1,16 @@
+package algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class N10773 {
+    // https://www.acmicpc.net/problem/10773
+    // 스택 관련 문제
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+
+
+}
