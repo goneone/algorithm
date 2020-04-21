61fc1ce33f0bc93f0eaf3959a3249b2a73c68fb6
goneone <kuyt1819@gmail.com>
2020-04-21 23:06:07 +0900

    stack 문제

--- src/algorithm/N10773.java
+++ src/algorithm/N10773.java
@@ -9,8 +9,23 @@ public class N10773 {
     // 스택 관련 문제
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int K = Integer.parseInt(br.readLine());
+        java.util.Stack<Integer> stack = new java.util.Stack<Integer>();
+
+        for (int i = 0; i < K ; i++) {
+            int number =  Integer.parseInt(br.readLine());
+            if(number == 0 && stack.isEmpty() == false) {
+                stack.pop();
+            } else if(number != 0 ){
+                stack.push(number);
+            }
+        }
+        //sum
+        int sum = 0;
+        while(!stack.isEmpty()) {
+            System.out.println(sum);
+        }
 
     }
-
-
 }
+
