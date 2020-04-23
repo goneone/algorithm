401c1751e40e16a5552b5b55d9558673b93e969b
goneone <kuyt1819@gmail.com>
2020-04-24 00:55:20 +0900

    N9012

--- src/algorithm/N9012.java
+++ src/algorithm/N9012.java
@@ -2,29 +2,37 @@ package algorithm;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Stack;
+
 
 public class N9012 {
     //https://www.acmicpc.net/problem/9012
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
-        String value = "";
+        String input = "";
 
         for (int i = 0; i < T; i++) {
-            Stack tempStack = new Stack();
-            value = br.readLine();
-            for (int j = 0; j < value.length(); j++) {
-                if(value.substring(j, j+1) == "(") {
-                    tempStack.push(1);
+            boolean isVPS = true;
+            java.util.Stack<Character> tempStack = new Stack<Character>();
+            //java.util.Stack tempStack = new Stack();
+            input = br.readLine();
+            for (int j = 0; j < input.length(); j++) {
+                if(input.charAt(j) == '(') {
+                    tempStack.push(input.charAt(j));
                 } else {
-                    if(tempStack.size() == 0) {
-                        break;
-                    } else {
+                    if(!tempStack.empty()) {
                         tempStack.pop();
+                    } else {
+                        isVPS = false;
+                        break;
                     }
                 }
             }
-            if(tempStack.size() == 0) {
+            if(!tempStack.isEmpty()) {
+                isVPS = false;
+            }
+            if(isVPS) {
                 System.out.println("YES");
             } else {
                 System.out.println("NO");
