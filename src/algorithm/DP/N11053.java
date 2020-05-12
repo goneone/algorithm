5bc82ca6f49b75ad4576e398ed431a066dfc6559
goneone <kuyt1819@gmail.com>
2020-05-12 23:01:36 +0900

    c

--- src/algorithm/DP/N11053.java
+++ src/algorithm/DP/N11053.java
@@ -3,6 +3,8 @@ package algorithm.DP;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 
 public class N11053 {
     //////////////////////푸는중
@@ -10,12 +12,27 @@ public class N11053 {
         //가장 긴 증가하는 부분 수열의 길이
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        String[] A = new String[N];
-        //int A = new int[N];
-        A = br.readLine().split(" ");
-
-        // dp = 해당 수에 들어갈 수있는 가장 긴 증가하는 부분 수열의 개수
+        int[] A = new[N];
+        int[] DP = new[N]; //DP = 해당 수에 들어갈 수있는 가장 긴 증가하는 부분 수열의 개수
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
 
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+            DP[i] = 1;
+            for(int j= 0; j < i; j++) {
+                if(A[j] < A[i] && DP[i] <= DP[j]) {
+                    DP[i] = DP[j] + 1;
+                }
+            }
+        }
+
+        int max = 0;
+        for (int i = 0; i < N; i++) {
+            if(DP[i] > max) {
+                max = DP[i];
+            }
+        }
+        System.out.println(max);
     }
 }
